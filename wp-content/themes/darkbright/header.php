78957423b1149685b286db5920d4dd11effaebd3
yichen <yichenzhu1337@gmail.com>
2020-10-12 17:01:57 -0400

    reinit: new landing pag

--- wp-content/themes/darkbright/header.php
+++
@@ -1,3 +0,0 @@
-<meta charset="utf-8">
-<title> My awesome site baby: <?php wp_head() ?></title>
-<!--<link rel="stylesheet" href="--><?php //echo bloginfo('template_url'); ?><!--/style.css" type="text/css">-->
--- wp-content/themes/darkbright/sidebar.php
+++
@@ -1,7 +0,0 @@
-<?php /* Template for the main side bar */ ?>
-
-<?php if(is_active_sidebar('main-sidebar')): ?>
-	<aside class="sidebar widget-area">
-		<?php dynamic_sidebar('main-sidebar'); ?>
-	</aside>
-<?php endif; ?>
